04609729cf60a73f0f4fb05a5de39b6ab69d6867
RenanVitorazzi <renan.vitorazzi1@gmail.com>
2023-07-25 17:01:22 -0300

    att

--- app/Http/Requests/RecebimentosRequestStore.php
+++ app/Http/Requests/RecebimentosRequestStore.php
@@ -24,7 +24,7 @@ class RecebimentosRequestStore extends FormRequest
      */
     public function rules()
     {
-        $forma_pagamento = ['Pix', 'Depósito', 'Dinheiro', 'TED', 'DOC'];
+        $forma_pagamento = ['Pix', 'Depósito', 'Dinheiro', 'TED', 'DOC', 'Cheque'];
 
         return [
             'data' => ['date', 'required'],
